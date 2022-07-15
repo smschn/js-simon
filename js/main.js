f01f1f6babb88d95184c24f575635c18f519bb25
Luca Somaschini <somas99@gmail.com>
2022-07-15 16:10:30 +0200

    Added 5 prompts

--- js/main.js
+++ js/main.js
@@ -2,34 +2,56 @@
 const fiveValidRandomNumbers = [];
 const nMin = 1;
 const nMax = 100;
+const fiveUserAnswers = [];
 
 // ottenere 5 numeri univoci e stamparli in pagina al caricamento della stessa
 getRandomUniqueNumber(fiveValidRandomNumbers, nMin, nMax);
 document.getElementById('bottom_content').innerHTML = fiveValidRandomNumbers;
 
-// dopo 10 secondi i numeri scompaiono
+// dopo 5 secondi i numeri scompaiono
 setTimeout(function() {
 
     document.getElementById('bottom_content').innerHTML = '';
 
-}, 10000);
+}, 5000);
+
+// dopo 7 secondi appaiono 5 prompt
+setTimeout(function() {
+
+    let num1 = prompt('Inserisci il primo numero');
+    fiveUserAnswers.push(num1);
+    let num2 = prompt('Inserisci il secondo numero');
+    fiveUserAnswers.push(num2);
+    let num3 = prompt('Inserisci il primo numero');
+    fiveUserAnswers.push(num3);
+    let num4 = prompt('Inserisci il primo numero');
+    fiveUserAnswers.push(num4);
+    let num5 = prompt('Inserisci il primo numero');
+    fiveUserAnswers.push(num5);
+
+    console.log(fiveUserAnswers + 'post')
+
+}, 7000);
+
+console.log(fiveUserAnswers)
 
 // --- inizio funzioni
 
 // funzione che genera numeri casuali
 function getRandomNumber(nMinFunc, nMaxFunc) {
+
     const randomNumber = Math.floor(Math.random() * (nMaxFunc - nMinFunc + 1) + nMinFunc)
     return randomNumber;
+
 }
 
 // funzione per ottenere numeri univoci
-
 function getRandomUniqueNumber(arrayF, minF, maxF) {
+
     let randomNumberGenerated;
     let check = false;
     while (check == false) {
         randomNumberGenerated = getRandomNumber(minF, maxF);
-        // console.log(randomNumberGenerated);
         if (arrayF.includes(randomNumberGenerated) == false) {
             arrayF.push(randomNumberGenerated);
         }
@@ -37,6 +59,6 @@ function getRandomUniqueNumber(arrayF, minF, maxF) {
             check = true;
         }
     }
-    // console.log(fiveValidRandomNumbers);
     return arrayF;
+
 }
